2fc217d8ab2bee526fb220aa04c6853b58162b35
Maksim Shahov <utypostev@gmail.com>
2025-10-29 21:37:44 +0300

    ringbuffer interface + minor main rework

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <gtest/gtest.h>
 
 int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+
+#ifdef TESTS
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+#else
+
+    return 0;
+#endif
 }
