e20688d4889837031b2238d2d39b7cc2f15fed62
iltl <shahov.maksim.i@gmail.com>
2025-10-30 18:07:05 +0300

    minor changes

---
+++ datastructures/tests/ringbuffer_test.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+#include "datastructures/ringbuffer/ringbuffer.hpp"
+
+TEST(Ringbuffer, Basic) {
+    RingBuffer<int> rb;
+
+    // rb.push_back(1);
+    // rb.push_back(2);
+    // rb.push_back(3);
+    
+    // std::cout << "q: ";
+    // for (; !q.empty(); q.pop_front())
+    //     std::cout << q.front() << ' ';
+    // std::cout << std::endl;
+    // assert(q.size() == 0);
+}
