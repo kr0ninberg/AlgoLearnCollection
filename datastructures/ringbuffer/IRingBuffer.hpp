91f4737f92f66342b44e382dc2e7f9e5daf82811
Maksim Shahov <utypostev@gmail.com>
2025-10-31 18:12:13 +0300

    NoParallelRingBuffer stub

--- datastructures/ringbuffer/IRingBuffer.hpp
+++ datastructures/ringbuffer/IRingBuffer.hpp
@@ -1,29 +1,29 @@
 #include <cstddef>
 
-  // ringbuffer types gonna be
-  // NO_MULTITHREADING
-  // LOCKFULL
-  // LOCKLESS_NO_ALIGN // false sharing case
-  // LOCKLESS_SIMPLE_ALIGN 
-  // LOCKLESS_COMPLEX_ALIGN
+// ringbuffer types gonna be
+// NO_MULTITHREADING
+// LOCKFULL
+// LOCKLESS_NO_ALIGN // false sharing case
+// LOCKLESS_SIMPLE_ALIGN 
+// LOCKLESS_COMPLEX_ALIGN
 
 template <typename T> class IRingBuffer {
 public:
-  virtual ~IRingBuffer() = default;
+    virtual ~IRingBuffer() = default;
 
-  // Element access
-  virtual T &front() = 0;
-  virtual const T &front() const = 0;
-  virtual T &back() = 0;
-  virtual const T &back() const = 0;
+    // Element access
+    virtual T &front() = 0;
+    virtual const T &front() const = 0;
+    virtual T &back() = 0;
+    virtual const T &back() const = 0;
 
-  // Modifiers
-  virtual void pop_front() = 0;
-  virtual bool push_back(const T &value) = 0;
-  virtual bool push_back(T &&value) = 0;
+    // Modifiers
+    virtual void pop_front() = 0;
+    virtual bool push_back(const T &value) = 0;
+    virtual bool push_back(T &&value) = 0;
 
-  // Capacity
-  virtual bool empty() const noexcept = 0;
-  virtual bool full() const noexcept = 0;
-  virtual std::size_t size() const noexcept = 0;
+    // Capacity
+    virtual bool empty() const noexcept = 0;
+    virtual bool full() const noexcept = 0;
+    virtual std::size_t size() const noexcept = 0;
 };
--- datastructures/ringbuffer/NoParallelRingBuffer.hpp
+++ datastructures/ringbuffer/NoParallelRingBuffer.hpp
@@ -1,22 +1,34 @@
 #include "IRingBuffer.hpp"
+#include <cstddef>
+#include <vector>
 
 template <typename T> class NoParallelRingBuffer : public IRingBuffer<T> {
 public:
-  virtual ~NoParallelRingBuffer() = default;
+    virtual ~NoParallelRingBuffer() = default;
+    NoParallelRingBuffer(std::size_t size) : _size(size), 
+                                             _buffer(size)
+    {}
 
-  // Element access
-  virtual T &front() override;
-  virtual const T &front() const override;
-  virtual T &back() override;
-  virtual const T &back() const override;
+    // Element access
+    virtual T &front() override;
+    virtual const T &front() const override;
+    virtual T &back() override;
+    virtual const T &back() const override;
 
-  // Modifiers
-  virtual void pop_front() override;
-  virtual bool push_back(const T &value) override;
-  virtual bool push_back(T &&value) override;
+    // Modifiers
+    virtual void pop_front() override;
+    virtual bool push_back(const T &value) override;
+    virtual bool push_back(T &&value) override;
 
-  // Capacity
-  virtual bool empty() const noexcept override;
-  virtual bool full() const noexcept override;
-  virtual std::size_t size() const noexcept override;
+    // Capacity
+    virtual bool empty() const noexcept override;
+    virtual bool full() const noexcept override;
+    virtual std::size_t size() const noexcept override;
+
+private:
+    std::size_t _r = 0 ,
+                _w = 0 ,
+                _size = 0,
+                _length = 0; 
+    std::vector<T> _buffer;
 };
