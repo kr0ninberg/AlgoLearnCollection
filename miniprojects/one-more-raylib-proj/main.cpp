bbf9bf664822101013ee0d0d79f15d71bacf0471
iltl <shahov.maksim.i@gmail.com>
2026-06-24 17:01:54 +0300

    another raylib proj

---
+++ miniprojects/one-more-raylib-proj/main.cpp
@@ -0,0 +1,23 @@
+#include <raylib.h>
+#include "sources/Simulation.h"
+
+#include "raylib.h"
+
+int main(void)
+{
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
+    {
+        ClearBackground(RAYWHITE);
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}}
